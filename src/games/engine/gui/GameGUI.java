739dc14e4a50f62d8c45af58fd44003786a65ea9
kkampfen <kkampfen@outlook.com>
2015-11-09 21:07:27 -0800

    Moved GUI to separate package

---
+++ src/games/engine/gui/GameGUI.java
@@ -0,0 +1,16 @@
+package games.engine.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by kkampfen on 11/9/2015.
+ */
+public class GameGUI extends JFrame {
+
+    public GameGUI() {
+        int width = 800;
+        int height = 600;
+
+        this.setSize(width, height);
+    }
+}
